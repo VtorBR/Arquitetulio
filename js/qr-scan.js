b3931f26f8e0ef372d5b2b4dff76eeee6c958c54
Vítor Fernandes <vitor_resende@ymail.com>
2018-08-26 00:25:03 -0300

    Change model on runtime

--- js/qr-scan.js
+++ js/qr-scan.js
@@ -1,17 +1,31 @@
 function StartQrScan()
 {
 	var video = document.querySelector('video');
+	var modelName;
 	if (video)
 	{
-		QCodeDecoder().decodeFromVideo(video, function(err, result) {
-			if (!err)
-			{
-				alert(result);
-			}
-		}, true);
+		QCodeDecoder().decodeFromVideo(video, ScanQrCode, false);
 	}
 	else
 	{
 		setTimeout(StartQrScan, 1000);
 	}
+}
+
+function ScanQrCode(error, result)
+{
+	if (!error && result != modelName)
+	{
+		modelName = result;
+		alert(modelName);
+		ChangeModel(modelName);
+		console.log(modelName);
+	}
+}
+
+function ChangeModel(name)
+{
+	var model = document.querySelector('a-collada-model ');
+	model.setAttribute('collada-model', 'url(' + name + ')');
+	model.setAttribute("scale", '0.1 0.1 0.1');
 }
